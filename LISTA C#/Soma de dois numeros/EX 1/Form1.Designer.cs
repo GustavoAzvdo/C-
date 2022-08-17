e67d9599ea731a79ca3c1f74cde671014c308187
Gustavo Azevedo <105758163+GustavoAzvdo@users.noreply.github.com>
2022-08-17 11:23:23 -0300

    Exercícios C#

---
+++ LISTA C#/Soma de dois numeros/EX 1/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace EX_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelN1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtN1 = new System.Windows.Forms.TextBox();
+            this.txtN2 = new System.Windows.Forms.TextBox();
+            this.txtSomar = new System.Windows.Forms.TextBox();
+            this.btnSomar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelN1
+            // 
+            this.labelN1.AutoSize = true;
+            this.labelN1.Location = new System.Drawing.Point(67, 104);
+            this.labelN1.Name = "labelN1";
+            this.labelN1.Size = new System.Drawing.Size(24, 13);
+            this.labelN1.TabIndex = 0;
+            this.labelN1.Text = "N1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(67, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "N2:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(67, 227);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Soma:";
+            // 
+            // txtN1
+            // 
+            this.txtN1.Location = new System.Drawing.Point(110, 97);
+            this.txtN1.Name = "txtN1";
+            this.txtN1.Size = new System.Drawing.Size(100, 20);
+            this.txtN1.TabIndex = 3;
+            // 
+            // txtN2
+            // 
+            this.txtN2.Location = new System.Drawing.Point(110, 138);
+            this.txtN2.Name = "txtN2";
+            this.txtN2.Size = new System.Drawing.Size(100, 20);
+            this.txtN2.TabIndex = 4;
+            // 
+            // txtSomar
+            // 
+            this.txtSomar.Location = new System.Drawing.Point(110, 220);
+            this.txtSomar.Name = "txtSomar";
+            this.txtSomar.Size = new System.Drawing.Size(100, 20);
+            this.txtSomar.TabIndex = 5;
+            // 
+            // btnSomar
+            // 
+            this.btnSomar.Location = new System.Drawing.Point(110, 177);
+            this.btnSomar.Name = "btnSomar";
+            this.btnSomar.Size = new System.Drawing.Size(100, 23);
+            this.btnSomar.TabIndex = 6;
+            this.btnSomar.Text = "Somar";
+            this.btnSomar.UseVisualStyleBackColor = true;
+            this.btnSomar.Click += new System.EventHandler(this.btnSomar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSomar);
+            this.Controls.Add(this.txtSomar);
+            this.Controls.Add(this.txtN2);
+            this.Controls.Add(this.txtN1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelN1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelN1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtN1;
+        private System.Windows.Forms.TextBox txtN2;
+        private System.Windows.Forms.TextBox txtSomar;
+        private System.Windows.Forms.Button btnSomar;
+    }
+}
+
---
+++ LISTA C#/Soma de dois numeros/EX 1/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EX_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        double n1, n2, somar;
+        private void btnSomar_Click(object sender, EventArgs e)
+        {
+            n1 = double.Parse(txtN1.Text);
+            n2 = double.Parse(txtN2.Text);
+
+            somar = n1 + n2;
+
+            txtSomar.Text = somar.ToString();
+        }
+    }
+}
